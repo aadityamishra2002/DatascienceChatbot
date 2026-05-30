d291027faff179537583291babf6f107a3575001
aaditya mishra <aadityamishra1063@gmail.com>
2026-05-30 15:22:17 +0530

    Fix import statement for CharacterTextSplitter

--- Chatbot.py
+++ Chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_community.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain.chains import RetrievalQA
 from PyPDF2 import PdfReader
 
